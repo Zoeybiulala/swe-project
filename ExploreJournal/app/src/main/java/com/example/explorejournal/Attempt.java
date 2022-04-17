43a9c03d80338065a9c1fce4ea7109d517d62fe2
Trang Dang <huyentrangdang160801@gmail.com>
2022-04-17 10:55:10 -0400

    working attempt view

---
+++ ExploreJournal/app/src/main/java/com/example/explorejournal/Attempt.java
@@ -0,0 +1,50 @@
+package com.example.explorejournal;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Attempt {
+    private String note;
+    private Date date;
+    private double rating;
+
+    public Attempt(String note, Date date, double rating) {
+        this.note = note;
+        this.date = date;
+        this.rating = rating;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Attempt{" +
+                "date=" + date +
+                ", rating=" + rating +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Attempt)) return false;
+        Attempt attempt = (Attempt) o;
+        return Double.compare(attempt.getRating(), getRating()) == 0 && getNote().equals(attempt.getNote()) && getDate().equals(attempt.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNote(), getDate(), getRating());
+    }
+}
--- ExploreJournal/app/src/main/java/com/example/explorejournal/RecipeAttemptActivity.java
+++ ExploreJournal/app/src/main/java/com/example/explorejournal/RecipeAttemptActivity.java
@@ -1,9 +1,16 @@
 package com.example.explorejournal;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class RecipeAttemptActivity extends BaseActivity {
+    private ArrayList<Attempt> attemptsArrayList;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,5 +31,23 @@ public class RecipeAttemptActivity extends BaseActivity {
         recipeDescription.setText(description);
 
         // display attempts
+        listView = findViewById(R.id.attempt_list_view);
+
+        attemptsArrayList = populateList();
+
+        AttemptAdapter attemptsAdapter = new AttemptAdapter(this,attemptsArrayList);
+        listView.setAdapter(attemptsAdapter);
     }
+    private ArrayList<Attempt> populateList(){
+
+        ArrayList<Attempt> list = new ArrayList<>();
+
+        for(int i = 0; i < 7; i++){
+            Attempt attempt = new Attempt("good", Calendar.getInstance().getTime(), 1000);
+            list.add(attempt);
+        }
+
+        return list;
+    }
+    
 }
